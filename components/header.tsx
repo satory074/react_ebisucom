b300b94a9d264819787d2372e780239dbb197356
satory074 <satory074@gmail.com>
2022-11-24 02:02:13 +0900

    chapter 3

--- components/header.tsx
+++ components/header.tsx
@@ -2,10 +2,10 @@ import Nav from "components/nav";
 import Logo from "./logo";
 
 export default function Header() {
-    return (
-        <header>
-            <Logo />
-            <Nav />
-        </header>
-    );
+  return (
+    <header>
+      <Logo boxOn />
+      <Nav />
+    </header>
+  );
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,23 +1,18 @@
 import Link from "next/link";
+import styles from "styles/nav.module.css";
 
 export default function Nav() {
   return (
     <nav>
-      <ul>
+      <ul className={styles.list}>
         <li>
-          <Link href="/">
-            Home
-          </Link>
+          <Link href="/">Home</Link>
         </li>
         <li>
-          <Link href="/about">
-            About
-          </Link>
+          <Link href="/about">About</Link>
         </li>
         <li>
-          <Link href="/contact">
-            Contact
-          </Link>
+          <Link href="/blog">Blog</Link>
         </li>
       </ul>
     </nav>
